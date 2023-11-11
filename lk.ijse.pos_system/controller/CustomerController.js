8a214f8fba96001b9e8f2af236e349c545ee5489
Your Name <you@example.com>
2023-11-11 15:26:56 +0530

    add validations

--- lk.ijse.pos_system/controller/CustomerController.js
+++ lk.ijse.pos_system/controller/CustomerController.js
@@ -4,6 +4,8 @@ import {setCustomerIds} from "./OrderController.js";
 
 
 var row_index = null;
+const contact_regex = new RegExp("^(?:0|94|\\+94|0094)?(?:(11|21|23|24|25|26|27|31|32|33|34|35|36|37|38|41|45|47|51|52|54|55|57|63|65|66|67|81|91)(0|2|3|4|5|7|9)|7(0|1|2|4|5|6|7|8)\\d)\\d{6}$");
+
 const clear = () => {
     $("#customerId").val("");
     $("#customerName").val("");
@@ -24,16 +26,51 @@ $("#customerButton>button[type='button']").eq(0).on("click", () =>{
     let customer_address = $("#customerAddress").val();
     let customer_contact = $("#contactNo").val();
 
-    console.log(customer_id,customer_name);
+    if (validate(customer_id,'customer Id') && validate(customer_name,'customer name') &&
+        validate(customer_address,'customer address') &&validate(customer_contact,'customer contact')) {
+        if (customer_contact.match(contact_regex)) {
 
-    let customer_obj = new CustomerModel(customer_id,customer_name,customer_address,customer_contact);
+            if (getCustomerIndex(customer_id) < 0) {
 
-    customer_db.push(customer_obj);
+                Swal.fire({
+                    title: 'Do you want to save the changes?',
+                    showDenyButton: true,
+                    confirmButtonText: 'Save',
+                    denyButtonText: `Don't save`,
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        console.log(customer_id, customer_name);
 
-    loadStudentData();
-    setCustomerIds();
+                        let customer_obj = new CustomerModel(customer_id, customer_name, customer_address, customer_contact);
 
-    clear();
+                        customer_db.push(customer_obj);
+
+                        loadStudentData();
+                        setCustomerIds();
+
+                        clear();
+
+                        Swal.fire('Customer Saved!', '', 'success');
+
+                    } else if (result.isDenied) {
+                        Swal.fire('Changes are not saved', '', 'info')
+                    }
+                });
+
+
+            } else {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Customer is already exists 😔',
+                });
+            }
+        }else{
+            Swal.fire({
+                icon: 'error',
+                title: 'Contact number is not valid! 😔',
+            });
+        }
+    }
 
 })
 
@@ -44,29 +81,90 @@ $("#customerButton>button[type='button']").eq(1).on("click", () =>{
     let customer_address = $("#customerAddress").val();
     let customer_contact = $("#contactNo").val();
 
-    let customer_obj = new CustomerModel(customer_id,customer_name,customer_address,customer_contact);
+    if (validate(customer_id,'customer Id') && validate(customer_name,'customer name') &&
+        validate(customer_address,'customer address') &&validate(customer_contact,'customer contact')) {
 
-    let index = customer_db.findIndex(item => item.customer_id === customer_id);
+        if (customer_contact.match(contact_regex)) {
 
-    customer_db[index] = customer_obj;
+            let customer_obj = new CustomerModel(customer_id, customer_name, customer_address, customer_contact);
 
-    loadStudentData();
+            let index = customer_db.findIndex(item => item.customer_id === customer_id);
 
-    clear();
+            if (index >= 0) {
+                Swal.fire({
+                    title: 'Do you want to update the customer?',
+                    showDenyButton: true,
+                    confirmButtonText: 'Update',
+                    denyButtonText: `Don't update`,
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        customer_db[index] = customer_obj;
+
+                        loadStudentData();
+
+                        clear();
+
+                        Swal.fire('Customer Updated!', '', 'success');
+
+                    } else if (result.isDenied) {
+                        Swal.fire('Changes are not updated!', '', 'info')
+                    }
+                });
+
+
+            } else {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Customer did not exists 😓',
+                });
+            }
+        }else{
+            Swal.fire({
+                icon: 'error',
+                title: 'Contact number is not valid! 😔',
+            });
+        }
+    }
 })
 
 //delete
 $("#customerButton>button[type='button']").eq(2).on("click", () =>{
     let customer_id = $("#customerId").val();
 
-    let index = customer_db.findIndex(item => item.customer_id === customer_id);
+    if (validate(customer_id,'customer Id')){
 
-    customer_db.splice(index, 1);
+        let index = customer_db.findIndex(item => item.customer_id === customer_id);
 
-    loadStudentData();
-    setCustomerIds();
+        if (index >= 0) {
+            Swal.fire({
+                title: 'Are you sure?',
+                text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    customer_db.splice(index, 1);
 
-    clear();
+                    loadStudentData();
+                    setCustomerIds();
+
+                    clear();
+
+                    Swal.fire('Deleted!', 'Your file has been deleted.', 'success');
+                }
+            });
+
+
+        }else{
+            Swal.fire({
+                icon: 'error',
+                title: 'Customer did not exists 😓',
+            });
+        }
+    }
 })
 $("#customer-tbl-body").on("click", "tr", function () {
     row_index = $(this).index();
@@ -81,3 +179,18 @@ $("#customer-tbl-body").on("click", "tr", function () {
     $('#customerAddress').val(customer_address);
     $('#contactNo').val(customer_contact);
 })
+
+function validate(value, field_name){
+    if (!value){
+        Swal.fire({
+            icon: 'warning',
+            title: `Please enter the ${field_name}!`
+    });
+        return false;
+    }
+    return true;
+}
+
+const getCustomerIndex = function (customerId) {
+    return customer_db.findIndex(customer => customer.customer_id === customerId);
+}
--- lk.ijse.pos_system/controller/ItemController.js
+++ lk.ijse.pos_system/controller/ItemController.js
@@ -1,22 +1,47 @@
 import {ItemModel} from "../model/ItemModel.js";
 import {customer_db, item_db} from "../db/db.js";
-import {setItemIds} from "./OrderController.js";
+import {setCustomerIds, setItemIds} from "./OrderController.js";
 
 var row_index = null;
 $("#itemButton>button[type='button']").eq(0).on("click", () =>{
    let item_code = $("#itemId").val();
    let item_name = $("#itemName").val();
-   let item_qty = $("#itemQty").val();
-   let item_price = $("#itemPrice").val();
+   let item_qty = parseInt($("#itemQty").val());
+   let item_price = parseFloat($("#itemPrice").val());
 
-   let item_obj = new ItemModel(item_code,item_name,item_qty,item_price);
+   if (validate(item_code,'item code') && validate(item_name,'item name') &&
+       validate(item_qty,'item qty') && validate(item_price,'item price')) {
 
-   item_db.push(item_obj);
+      if (getItemIndex(item_code) < 0) {
+         Swal.fire({
+            title: 'Do you want to save the changes?',
+            showDenyButton: true,
+            confirmButtonText: 'Save',
+            denyButtonText: `Don't save`,
+         }).then((result) => {
+            if (result.isConfirmed) {
+               let item_obj = new ItemModel(item_code, item_name, item_qty, item_price);
 
-    loadStudentData();
-   setItemIds();
+               item_db.push(item_obj);
 
-   clear();
+               loadStudentData();
+               setItemIds();
+
+               clear();
+
+               Swal.fire('Customer Saved!', '', 'success');
+
+            } else if (result.isDenied) {
+               Swal.fire('Changes are not saved', '', 'info')
+            }
+         });
+      }else{
+         Swal.fire({
+            icon: 'error',
+            title: 'Item is already exists 😔',
+         });
+      }
+   }
 
 });
 
@@ -42,29 +67,80 @@ $("#itemButton>button[type='button']").eq(1).on("click", () =>{
    let item_qty = $("#itemQty").val();
    let item_price = $("#itemPrice").val();
 
-   let item_obj = new ItemModel(item_code,item_name,item_qty,item_price);
+   if (validate(item_code,'item code') && validate(item_name,'item name') &&
+       validate(item_qty,'item qty') && validate(item_price,'item price')) {
 
-   let index = item_db.findIndex(item => item.item_code === item_code);
+       let item_obj = new ItemModel(item_code, item_name, item_qty, item_price);
 
-   item_db[index] = item_obj;
+       let index = item_db.findIndex(item => item.item_code === item_code);
 
-   loadStudentData();
+      if (index >= 0) {
 
-   clear();
+         Swal.fire({
+            title: 'Do you want to update the item?',
+            showDenyButton: true,
+            confirmButtonText: 'Update',
+            denyButtonText: `Don't update`,
+         }).then((result) => {
+            if (result.isConfirmed) {
+               item_db[index] = item_obj;
+
+               loadStudentData();
+
+               clear();
+
+               Swal.fire('Customer Updated!', '', 'success');
+
+            } else if (result.isDenied) {
+               Swal.fire('Changes are not updated!', '', 'info')
+            }
+         });
+      }else{
+         Swal.fire({
+            icon: 'error',
+            title: 'Item did not exists 😓',
+         });
+      }
+   }
 
 });
 
 $("#itemButton>button[type='button']").eq(2).on("click", () => {
    let item_code = $("#itemId").val();
 
-   let index = item_db.findIndex(item => item.item_code === item_code);
+   if(validate(item_code,'item code')) {
 
-   item_db.splice(index,1);
+      let index = item_db.findIndex(item => item.item_code === item_code);
 
-   loadStudentData();
-   setItemIds();
+      if (index >= 0) {
 
-   clear();
+         Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+         }).then((result) => {
+            if (result.isConfirmed) {
+               item_db.splice(index, 1);
+
+               loadStudentData();
+               setItemIds();
+
+               clear();
+
+               Swal.fire('Deleted!', 'Your file has been deleted.', 'success');
+            }
+         });
+      }else{
+         Swal.fire({
+            icon: 'error',
+            title: 'Item did not exists 😓',
+         });
+      }
+   }
 
 })
 $("#item_table_body").on("click","tr", function () {
@@ -80,3 +156,18 @@ $("#item_table_body").on("click","tr", function () {
    $("#itemQty").val(item_qty);
    $("#itemPrice").val(item_price);
 })
+
+function validate(value, field_name){
+   if (!value){
+      Swal.fire({
+         icon: 'warning',
+         title: `Please enter the ${field_name}!`
+      });
+      return false;
+   }
+   return true;
+}
+
+const getItemIndex = function (item_code) {
+   return item_db.findIndex(item => item.item_code === item_code);
+}
